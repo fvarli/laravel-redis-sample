c5eb608924abc8fa5f54012d6f71bf2295c43535
root <fvarlii@gmail.com>
2025-01-28 19:35:09 +0300

    implemented redis

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class ArticleController extends Controller
 {
     public function index()
     {
         return Article::all();
+    }
+    
+    public function articlesWithRedis(Article $article)
+    {
+        $redis = Redis::get('articles');
+
+        if ($redis) {
+            return json_decode($redis);
+        }
+
+        $articles = Article::all();
+        Redis::set('articles', $articles);
+        return $articles;
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::apiResource('articles', ArticleController::class);
+Route::get('articlesWithRedis', [ArticleController::class, 'articlesWithRedis']);
